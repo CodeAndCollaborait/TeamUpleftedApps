9a893e91bfd08d3245fa2a3abc6b483618c96727
Loryn Duncombe <lorynduncombe1@gmail.com>
2021-05-25 13:33:49 -0400

    Updted Person Controller to allow Push,Post,Delete, and Get

--- Loryn_Apps/SpringTutorials/src/main/java/com/spring/tutorial/SpringTutorials/personapi/controller/PersonController.java
+++ Loryn_Apps/SpringTutorials/src/main/java/com/spring/tutorial/SpringTutorials/personapi/controller/PersonController.java
@@ -1,15 +1,15 @@
 package com.spring.tutorial.SpringTutorials.personapi.controller;
 
+import com.spring.tutorial.SpringTutorials.api.model.Employee;
 import com.spring.tutorial.SpringTutorials.personapi.database.PersonDatabase;
 import com.spring.tutorial.SpringTutorials.personapi.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.lang.reflect.Array;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController()
@@ -28,5 +28,42 @@ public class PersonController {
     public Optional<Person> getPersonById
             (@PathVariable(value = "id") long id){
         return personDatabase.findById(id);
+    }
+
+    //Only when creating a new record
+    @PostMapping("/personlist")
+    //@RequestBody- allows you to insert employee into data.sql
+    public Person createNewPerson(@RequestBody Person person){
+        return personDatabase.save(person);
+
+
+    }
+
+    @PutMapping("/personlist/{id}")
+    public Person updateExistingPer(@RequestBody Person person, @PathVariable(value = "id") long id) throws Exception{
+        //combination of read and update
+
+        //Read
+        Person personDetails = personDatabase.findById(id).orElseThrow(()->new Exception("Person by this ID" + id + "not found in database"));
+
+        personDetails.setPersonName(person.getPersonName());
+        personDetails.setOccupation(person.getOccupation());
+        personDetails.setAge(person.getAge());
+        personDetails.setSiblings(person.getSiblings());
+        personDetails.setSign(person.getSign());
+
+        personDatabase.save(personDetails);
+        return personDetails;
+
+    }
+    @DeleteMapping("/personlist/{id}")
+    public Map<Long,String> removeEmpFromList(@PathVariable(value = "id") long id) throws Exception{
+        Person personDetails = personDatabase.findById(id).orElseThrow(()->new Exception("Emp by this ID" + id + "not found in database"));
+
+        personDatabase.delete(personDetails);
+        Map<Long,String> personDelete =new HashMap<>();
+        personDelete.put(id, "Person by this ID has been deleted");
+        return personDelete;
+
     }
 }
--- Loryn_Apps/SpringTutorials/src/main/java/com/spring/tutorial/SpringTutorials/personapi/database/PersonDatabase.java
+++
@@ -1,9 +0,0 @@
-package com.spring.tutorial.SpringTutorials.personapi.database;
-
-import com.spring.tutorial.SpringTutorials.personapi.model.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PersonDatabase extends JpaRepository<Person, Long> {
-}
