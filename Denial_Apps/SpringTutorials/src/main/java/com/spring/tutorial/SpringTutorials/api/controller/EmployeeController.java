df53ff3b4bb7ef14c1055a81e2120f9ad1a15a30
Daniel Alemu <dsa.alemu@gmail.com>
2021-05-26 10:19:23 -0400

    add, update and delete methods

--- Denial_Apps/SpringTutorials/src/main/java/com/spring/tutorial/SpringTutorials/api/controller/EmployeeController.java
+++ Denial_Apps/SpringTutorials/src/main/java/com/spring/tutorial/SpringTutorials/api/controller/EmployeeController.java
@@ -3,13 +3,12 @@ package com.spring.tutorial.SpringTutorials.api.controller;
 import com.spring.tutorial.SpringTutorials.api.database.EmployeeDatabase;
 import com.spring.tutorial.SpringTutorials.api.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.websocket.server.PathParam;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -18,6 +17,7 @@ public class EmployeeController {
     @Autowired
     private EmployeeDatabase employeeDatabase;
 
+    // read a data -- @GetMapping
     @GetMapping("/emplist")
     public List<Employee> getListOfEmployee(){
         return employeeDatabase.findAll();
@@ -27,5 +27,45 @@ public class EmployeeController {
     public Optional<Employee> getEmpById(@PathVariable(value = "id") long id){
         return employeeDatabase.findById(id);
     }
+
+    // post a data @PostMapping --- when we create a new record
+    //@RequestBody allows to insert an Empoloyee object
+    @PostMapping("/emplist")
+    public Employee createNewEmployee(@RequestBody Employee employee){
+        return employeeDatabase.save(employee);
+    }
+
+    // update information
+    //@PutMapping is a combination of get and post method
+//    @PutMapping("/emplist")
+//    public Employee updateEmployee(@RequestBody Employee employee){
+//        return employeeDatabase.save(employee);
+//    }
+
+    @PutMapping("/emplist/{id}")
+    public Employee updateExistingEmployee(@RequestBody Employee employee, @PathVariable(value = "id") long id) throws Exception {
+        Employee updateEmployee = employeeDatabase.findById(id).orElseThrow(() -> new Exception("Emp by this ID " + id + " not found in database"));
+
+        updateEmployee.setEmpName(employee.getEmpName());
+        updateEmployee.setEmpDepartment(employee.getEmpDepartment());
+        updateEmployee.setEmpIncome(employee.getEmpIncome());
+        updateEmployee.setActive(employee.getActive());
+
+        employeeDatabase.save(updateEmployee);
+        return updateEmployee;
+        //return employeeDatabase.save(employee);
+    }
+
+
+    // delete @DeleteMapping
+    @DeleteMapping("emplist/{id}")
+    public Map<Long, String> deleteEmployeeFromList(@PathVariable(value = "id") long id) throws Exception{
+        Employee deleteEmployee = employeeDatabase.findById(id).orElseThrow(() -> new Exception("Emp by this ID " + id + " not found in database"));
+        employeeDatabase.delete(deleteEmployee);
+        Map<Long, String> deleteEmpResponse = new HashMap<>();
+        deleteEmpResponse.put(id, "Emp by this ID has been deleted ");
+        return deleteEmpResponse;
+    }
+
 
 }
--- Denial_Apps/SpringTutorials/src/main/java/com/spring/tutorial/SpringTutorials/personAPI/controller/PersonController.java
+++ Denial_Apps/SpringTutorials/src/main/java/com/spring/tutorial/SpringTutorials/personAPI/controller/PersonController.java
@@ -4,12 +4,11 @@ import com.spring.tutorial.SpringTutorials.api.model.Employee;
 import com.spring.tutorial.SpringTutorials.personAPI.database.PersonDatabase;
 import com.spring.tutorial.SpringTutorials.personAPI.model.Person;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -28,4 +27,34 @@ public class PersonController {
     public Optional<Person> getEmpById(@PathVariable(value = "id") long id){
         return personDatabase.findById(id);
     }
+
+    // add new person record
+    @PostMapping("/personlist")
+    public Person addNewPerson(@RequestBody Person person){
+        return personDatabase.save(person);
+    }
+
+    //update existing person record
+    @PutMapping("/personlist/{id}")
+    public Person updateEmployee(@RequestBody Person person, @PathVariable(value = "id") long id) throws Exception {
+        Person updatePerson = personDatabase.findById(id).orElseThrow(() -> new Exception("Person by this SSN  " + id + " not found in database"));
+        updatePerson.setAddress(person.getAddress());
+        updatePerson.setAge(person.getAge());
+        updatePerson.setFirstName(person.getFirstName());
+        updatePerson.setLastName(person.getLastName());
+        updatePerson.setTitle(person.getTitle());
+        return personDatabase.save(person);
+    }
+
+    //delete existing record
+    @DeleteMapping("/personlist/{id}")
+    public Map<Long,String> deleteEmployeeRecord(@PathVariable(value = "id") long id) throws Exception{
+        Person deletePerson = personDatabase.findById(id).orElseThrow(() -> new  Exception("Person by this SSN " + id + " not found in database"));
+
+        personDatabase.delete(deletePerson);
+        Map<Long, String> deleteResponse = new HashMap<>();
+        deleteResponse.put(id, "Person by this SSN has been deleted ");
+        return deleteResponse;
+    }
+
 }
